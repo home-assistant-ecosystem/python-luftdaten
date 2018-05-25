89c8faef02044665d07a8bbefff4385a27b55a8a
Fabian Affolter <fabian@affolter-engineering.ch>
2018-05-25 11:00:18 +0200

    Update for async and add docstring

--- luftdaten/__init__.py
+++ luftdaten/__init__.py
@@ -14,23 +14,9 @@ from . import exceptions
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = 'https://api.luftdaten.info/v1'
 
-VOLUME_MICROGRAMS_PER_CUBIC_METER = 'µg/m3'
-
-SENSOR_TEMPERATURE = 'temperature'
-SENSOR_HUMIDITY = 'humidity'
-SENSOR_PM10 = 'P1'
-SENSOR_PM2_5 = 'P2'
-
-SENSOR_TYPES = {
-    SENSOR_TEMPERATURE: ['Temperature', '°C'],
-    SENSOR_HUMIDITY: ['Humidity', '%'],
-    SENSOR_PM10: ['PM10', VOLUME_MICROGRAMS_PER_CUBIC_METER],
-    SENSOR_PM2_5: ['PM2.5', VOLUME_MICROGRAMS_PER_CUBIC_METER]
-}
-
 
 class Luftdaten(object):
-    """A class for handling connections from Luftdaten.info."""
+    """A class for handling the data retrieval."""
 
     def __init__(self, sensor_id, loop, session):
         """Initialize the connection."""
@@ -47,23 +33,22 @@ class Luftdaten(object):
             }
         self.meta = {}
 
-    @asyncio.coroutine
-    def async_get_data(self):
+    async def async_get_data(self):
+        """Retrieve the data."""
         url = '{}/{}/{}/'.format(_RESOURCE, 'sensor', self.sensor_id)
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
-                response = yield from self._session.get(url)
+                response = await self._session.get(url)
 
             _LOGGER.debug(
                 "Response from luftdaten.info: %s", response.status)
-            data = yield from response.json()
+            data = await response.json()
             _LOGGER.debug(data)
         except (asyncio.TimeoutError, aiohttp.ClientError):
             _LOGGER.error("Can not load data from luftdaten.info")
             raise exceptions.LuftdatenConnectionError()
 
-        print(data)
         try:
             sensor_data = sorted(
                 data, key=lambda timestamp: timestamp['timestamp'],
@@ -75,7 +60,8 @@ class Luftdaten(object):
                         self.values[measurement] = float(entry['value'])
 
             self.meta['sensor_id'] = self.sensor_id
-            self.meta['longitude'] = float(sensor_data['location']['longitude'])
+            self.meta['longitude'] = float(
+                sensor_data['location']['longitude'])
             self.meta['latitude'] = float(sensor_data['location']['latitude'])
         except (TypeError, IndexError):
             raise exceptions.LuftdatenError()
