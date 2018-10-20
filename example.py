89041f59d2bed1d3342a7af8652aec75c7c70e60
Fabian Affolter <mail@fabian-affolter.ch>
2018-10-20 16:24:02 +0200

    Update example

--- example.py
+++ example.py
@@ -16,11 +16,14 @@ async def main():
         data = Luftdaten(155, loop, session)
         await data.get_data()
 
-        # Print the sensor values
-        print("Sensor values:", data.values)
+        if data.values and data.meta:
+            # Print the sensor values
+            print("Sensor values:", data.values)
 
-        # Print the coordinates fo the sensor
-        print("Location:", data.meta['latitude'], data.meta['longitude'])
+            # Print the coordinates fo the sensor
+            print("Location:", data.meta['latitude'], data.meta['longitude'])
+        else:
+            print("Station is not available:", data.sensor_id)
 
 
 loop = asyncio.get_event_loop()
