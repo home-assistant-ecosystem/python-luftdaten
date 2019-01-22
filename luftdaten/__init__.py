6feb61ddf9b5a9f7b813cd44537cfcd61ffd381c
Fabian Affolter <mail@fabian-affolter.ch>
2019-01-22 23:38:35 +0100

    Update docstring

--- luftdaten/__init__.py
+++ luftdaten/__init__.py
@@ -1,8 +1,4 @@
-"""
-Copyright (c) 2017-2018 Fabian Affolter <fabian@affolter-engineering.ch>
-
-Licensed under MIT. All rights reserved.
-"""
+"""Wrapper to get the measurings from a Luftdaten station."""
 import asyncio
 import logging
 
