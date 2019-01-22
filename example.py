2d6abbe2d063811e8fce7f81a4feae1384f60a65
Fabian Affolter <mail@fabian-affolter.ch>
2019-01-22 23:37:27 +0100

    Update docstring

--- example.py
+++ example.py
@@ -1,8 +1,4 @@
-"""
-Copyright (c) 2017-2018 Fabian Affolter <fabian@affolter-engineering.ch>
-
-Licensed under MIT. All rights reserved.
-"""
+"""Example for getting the data from a station."""
 import asyncio
 
 import aiohttp
