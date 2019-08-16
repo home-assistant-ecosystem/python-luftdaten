85a50d23612cd243e3b7181488a2533dbcc0a325
Fabian Affolter <mail@fabian-affolter.ch>
2019-08-16 11:31:06 +0200

    Prepare for 0.6.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='luftdaten',
-    version='0.6.2',
+    version='0.6.3',
     description='Python API for interacting with luftdaten.info.',
     long_description=long_description,
     url='https://github.com/fabaff/python-luftdaten',
