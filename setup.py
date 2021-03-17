6bf3a70e13ec0da10ce9de2464f695878fe2435e
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-17 10:49:07 +0100

    Formatting with black

--- setup.py
+++ setup.py
@@ -9,32 +9,32 @@ except ImportError:
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name='luftdaten',
-    version='0.6.4',
-    description='Python API for interacting with luftdaten.info.',
+    name="luftdaten",
+    version="0.6.4",
+    description="Python API for interacting with luftdaten.info.",
     long_description=long_description,
-    url='https://github.com/fabaff/python-luftdaten',
-    download_url='https://github.com/fabaff/python-luftdaten/releases',
-    author='Fabian Affolter',
-    author_email='fabian@affolter-engineering.ch',
-    license='MIT',
-    install_requires=['aiohttp>=3.7.4,<4', 'async_timeout<4'],
-    packages=['luftdaten'],
+    url="https://github.com/fabaff/python-luftdaten",
+    download_url="https://github.com/fabaff/python-luftdaten/releases",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    license="MIT",
+    install_requires=["aiohttp>=3.7.4,<4", "async_timeout<4"],
+    packages=["luftdaten"],
     zip_safe=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities",
     ],
 )
