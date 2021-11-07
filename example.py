d12070da65690948460eae893d1ff6f592df2c60
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-07 12:22:06 +0100

    Migrate to httpx

--- example.py
+++ example.py
@@ -1,29 +1,26 @@
 """Example for getting the data from a station."""
 import asyncio
 
-import aiohttp
-
 from luftdaten import Luftdaten
 
-SENSOR_ID = 155
+SENSOR_ID = 152
 
 
 async def main():
     """Sample code to retrieve the data."""
-    async with aiohttp.ClientSession() as session:
-        data = Luftdaten(SENSOR_ID, loop, session)
-        await data.get_data()
+    data = Luftdaten(SENSOR_ID)
+    await data.get_data()
 
-        if not await data.validate_sensor():
-            print("Station is not available:", data.sensor_id)
-            return
+    if not await data.validate_sensor():
+        print("Station is not available:", data.sensor_id)
+        return
 
-        if data.values and data.meta:
-            # Print the sensor values
-            print("Sensor values:", data.values)
+    if data.values and data.meta:
+        # Print the sensor values
+        print("Sensor values:", data.values)
 
-            # Print the coordinates fo the sensor
-            print("Location:", data.meta['latitude'], data.meta['longitude'])
+        # Print the coordinates fo the sensor
+        print("Location:", data.meta["latitude"], data.meta["longitude"])
 
 
 loop = asyncio.get_event_loop()
