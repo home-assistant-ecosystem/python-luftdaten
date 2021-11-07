0ed2bd0655da5a23585df5d5dbe7ffb4e9a2b350
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-07 12:26:51 +0100

    Add initial tests

---
+++ tests/test_timeout.py
@@ -0,0 +1,24 @@
+"""Test the interaction with the Luftdaten API."""
+import httpx
+import pytest
+from pytest_httpx import HTTPXMock
+
+from luftdaten import Luftdaten
+
+SENSOR_ID = 1
+
+@pytest.mark.asyncio
+async def test_timeout(httpx_mock: HTTPXMock):
+    """Test if the connection is hitting the timeout."""
+
+    def raise_timeout(request, extensions: dict):
+        """Set the timeout for the requests."""
+        raise httpx.ReadTimeout(
+            f"Unable to read within {extensions['timeout']}", request=request
+        )
+
+    httpx_mock.add_callback(raise_timeout)
+
+    with pytest.raises(httpx.ReadTimeout):
+        client = Luftdaten(SENSOR_ID)
+        await client.get_data()
