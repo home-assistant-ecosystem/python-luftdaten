ed13dbb6d021e4e506222f632aa2f9029d0ba794
Fabian Affolter <fabian@affolter-engineering.ch>
2021-11-23 16:36:57 +0100

    Remove loop

--- example.py
+++ example.py
@@ -27,6 +27,6 @@ async def main():
             data.meta["altitude"],
         )
 
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+        
+if __name__ == "__main__":
+    asyncio.run(main())
